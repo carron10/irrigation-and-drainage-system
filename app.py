ff5fa396654ac961315e8e1f7fe5832f88b8a3d7
Carron Muleya <carronmuleya10@gmail.com>
2024-02-10 16:59:57 +0200

    My First Commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import request,render_template
+
+app = Flask(__name__,
+            static_url_path='', 
+            static_folder='static',
+            template_folder='templates')
+
+@app.route("/") 
+@app.route("/index.html") 
+def hello(): 
+    return render_template('index.html',  
+                           page="index")
+
+@app.route("/<page>") 
+def pages(page): 
+    return render_template(page+'.html',  
+                           page=page)
+if __name__=="__main__":
+    app.run(debug=True)
