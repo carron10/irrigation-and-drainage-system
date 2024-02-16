41c3920b23724b88558911de5253a6c8bf0d6773
Carron Muleya <carronmuleya10@gmail.com>
2024-02-16 06:57:35 +0200

    Add report page and setting page,change buttons, notification

--- app.py
+++ app.py
@@ -4,7 +4,13 @@ from flask import request,render_template
 app = Flask(__name__,
             static_url_path='', 
             static_folder='static',
+ 
             template_folder='templates')
+
+@app.route('/send_data')
+def getdata():
+    return "your data is saved suceee"
+
 
 @app.route("/") 
 @app.route("/index.html") 
